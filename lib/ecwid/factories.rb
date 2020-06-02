58b7a59a99627ddce44c7a4493accd733a5d6836
Marc Sangster <msangster@shopkeep.com>
2020-06-02 12:31:53 +0100

    initial commit

---
+++ lib/ecwid/factories.rb
@@ -0,0 +1,8 @@
+require "ecwid/factories/version"
+
+module Ecwid
+  module Factories
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/ecwid/factories/version.rb
@@ -0,0 +1,5 @@
+module Ecwid
+  module Factories
+    VERSION = "0.1.0"
+  end
+end
